4f05ae55043002882b09bdde6100c2ebe127cf59
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-12-03 20:32:56 +0900

    ignore追記

---
+++ src/main/java/com/example/study_org_server/StudyOrgServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_org_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOrgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyOrgServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_org_server/StudyOrgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_org_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOrgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
